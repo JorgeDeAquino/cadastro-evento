03d1043c78ca06d4ece2218622becef90c5f6e0e
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-01-11 19:39:35 -0300

    add rotas basicas

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,16 +6,44 @@
 
         <title>@yield('title')</title>
 
+        <!-- Fonte do Google -->
+        <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        <!-- CSS Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+        <!-- CSS da app -->
         <link rel="stylesheet" href="/css/styles.css">
         <script src="/js/scripts.js"></script>
 
     </head>
     <body >
-           
-    @yield('content')
-
-    <footer>
-        <p>Jorge  Alexandre &copy; 2023</p>
-    </footer>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="navbar-brand">
+                        <img src="" alt="Logo">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Criar Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Cadastrar</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        @yield('content')
+        <footer>
+            <p>Jorge  Alexandre &copy; 2023</p>
+        </footer>
     </body>
 </html>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,6 +5,9 @@
 @section('content')
 
 <h1>Essa é a pagina de produtos</h1>
+@if($busca != '')
+    <p>O usuario esta buscando por: {{ $busca }}</p>
+@endif
 <a href="/">voltar para home</a>
 
 @endsection
