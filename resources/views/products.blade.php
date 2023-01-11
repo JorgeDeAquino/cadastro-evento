16aa0229d4995086a030950b9dec98c8f4c6e506
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-01-11 17:20:06 -0300

    add layout basico

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,2 +1,10 @@
+@extends('layouts.main')
+
+@section('title', 'Produtos')
+
+@section('content')
+
 <h1>Essa é a pagina de produtos</h1>
 <a href="/">voltar para home</a>
+
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,41 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Laravel</title>
+@section('title', 'Home')
 
-        <link rel="stylesheet" href="/css/styles.css">
-        <script src="/js/scripts.js"></script>
+@section('content')
 
-    </head>
-    <body >
-            <h1>Home</h1>
-            @if(10 > 5)
-                <p>A condicao é true</p>
-            @endif
+<h1>Home</h1>
+@if(10 > 5)
+    <p>A condicao é true</p>
+@endif
 
 
-            <p>{{ $nome }}</p>
+<p>{{ $nome }}</p>
 
-            @if($nome == "Jao")
-                <p>O nome é Jao</p>
-            @elseif($nome == "Jorge")
-                <p>o nome é {{ $nome }} e ele tem {{ $idade }} anos, trabalha como {{ $profissao }}</p>
-            @else
-                <p>O nome nao é Jao</p>
-            @endif
+@if($nome == "Jao")
+    <p>O nome é Jao</p>
+@elseif($nome == "Jorge")
+    <p>o nome é {{ $nome }} e ele tem {{ $idade }} anos, trabalha como {{ $profissao }}</p>
+@else
+    <p>O nome nao é Jao</p>
+@endif
 
-            @for($i = 0; $i < count($arr); $i++)
-                <p>{{ $arr[$i] }} - {{ $i }}</p>
-                @if($i == 2)
-                    <p>o i é 2</p>
-                @endif
-            @endfor
+@for($i = 0; $i < count($arr); $i++)
+    <p>{{ $arr[$i] }} - {{ $i }}</p>
+    @if($i == 2)
+        <p>o i é 2</p>
+    @endif
+@endfor
 
-            @foreach($nomes as $nome)
-                <p>{{ $loop->index }}</p>
-                <p>{{ $nome }}</p>
-            @endforeach
+@foreach($nomes as $nome)
+    <p>{{ $loop->index }}</p>
+    <p>{{ $nome }}</p>
+@endforeach
 
-            {{-- inserir codigo php no blade esse comentario so aparece no blade--}}
-            @php
-                $name = "Juaum";
-                echo $name;
-            @endphp
-    </body>
-</html>
+{{-- inserir codigo php no blade esse comentario so aparece no blade--}}
+@php
+    $name = "Juaum";
+    echo $name;
+@endphp
+
+@endsection
