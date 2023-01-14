f3cf779bd3c26cf12ec423b330e3c612ccb18157
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-01-14 19:11:25 -0300

    add date na welcome e tmabem no show ambos no formato corredo padrao brasileiro

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
         @foreach($events as $event)
         <div class="card col-md-3">
             <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
-            <p class="card-date">07/03/2023</p>
+            <p class="card-date">{{ date('d/m/y', strtotime($event->date)) }}</p>
             <h5 class="card-title">{{ $event->title }}</h5>
             <p class="card-participants">X Participantes</p>
             <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
