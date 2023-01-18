a4f78834b2ab5fc2b00a8d442fea28f684e67b64
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-01-18 15:54:30 -0300

    add botao de confirmacao de presenca

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -15,6 +15,7 @@
             <p class="event-date"><ion-icon name="calendar-outline"></ion-icon> {{ date('d/m/Y', strtotime($event->date)) }} </p>
             <p class="events-participants"><ion-icon name="people-outline"></ion-icon> {{ count($event->users) }} Participantes </p>
             <p class="events-owner"><ion-icon name="star-outline"></ion-icon> {{ $eventOwner['name'] }} </p>
+            @if(!$hasUserJoined)
             <form action="/events/join/{{ $event->id}}" method="POST">
                 @csrf
                 <a href="/events/join/{{ $event->id}}" 
@@ -24,6 +25,9 @@
                 Confirmar Presença
                 </a>
             </form>
+            @else
+                <p class="already-joined-msg">Você já confirmou presença =D</p>
+            @endif
             <h3>O evento conta com:</h3>
             <ul id="items-list">
                 @foreach($event->items as $item)
